ac4aa53c67ded7d47dfa4a97d6b57f60a3ffe549
Lars Trieloff <lars@trieloff.net>
2024-10-08 10:38:38 +0200

    build(release): add semantic release

---
+++ .releaserc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }],
+    "@semantic-release/npm",
+    ["@semantic-release/git", {
+      "assets": [
+        "package.json",
+        "CHANGELOG.md",
+      ],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    ["@semantic-release/github", {
+    }],
+    ["semantic-release-slack-bot", {
+      notifyOnSuccess: true,
+      notifyOnFail: true,
+      markdownReleaseNotes: true,
+      slackChannel: "rum-explorers",
+    }],
+  ],
+  branches: ['main'],
+};
